1aef246e53613f72d2a818e7f391be4b9e665f36
jmontero <jmontero@ingenioso.co>
2025-05-27 17:25:02 -0500

    [UPD] Se actualiza metodo de generación de PDF para el envio en whatsapp, y mensaje de chatboot

--- subscription_whatsapp/models/sale_subscription.py
+++ subscription_whatsapp/models/sale_subscription.py
@@ -9,6 +9,7 @@ import html2text
 #import html2plaintext
 from odoo.exceptions import AccessError
 from odoo.addons.all_in_one_whatsapp_integration.wizard.whatsapp_mode_message import WhatsappModeMessage
+from markupsafe import Markup
 
 logger = logging.getLogger(__name__)
 
@@ -28,14 +29,23 @@ class SaleSubscription(models.Model):
                          
         if send_mode in ['whatsapp', 'both']:
             whatsapp_message_mode = WhatsappModeMessage(self.env)
-
-            
             mail_template = self.template_id.invoice_mail_template_id
             mail_template_values = mail_template.with_context(
-                tpl_partners_only=True                
+                tpl_partners_only=True
             )._generate_template([invoice.id], render_fields=["body_html"])
             html_message = mail_template_values.get(invoice.id, {}).get("body_html", "")
-            plain_text_message =  html2text.html2text(html_message)            
-            contenido_binario = base64.b64decode(invoice.invoice_pdf_report_file, validate=True)
+            plain_text_message =  html2text.html2text(html_message)
+
+            # Generar PDF de forma segura desde el reporte
+            contenido_binario, _ = self.env["ir.actions.report"]._render_qweb_pdf(
+            "account.account_invoices", invoice.id
+            )
+            # Trasnformarlo a base64  para enviarlo via whatsapp
             base64_limpio = base64.b64encode(contenido_binario).decode('utf-8')
             whatsapp_message_mode.action_send_attachment_document_message(invoice.partner_id.mobile,f"{invoice.name}.pdf", plain_text_message, base64_limpio)            
+            # Mensaje del boot 
+            self.message_post(
+                body=html_message,
+                subject="Factura enviada por WhatsApp",
+                message_type="comment"
+            )
